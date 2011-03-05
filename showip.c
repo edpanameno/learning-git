794ea8b96aeb0583271947eb449382a592aaeff0
Ed Panameno <ed.panameno@gmail.com>
2011-03-05 09:34:40 -0800

    Now have all curly braces on a new line ... the way I enjoy my source code to look like :)

--- showip.c
+++ showip.c
@@ -10,13 +10,14 @@
 #include <netdb.h>
 #include <arpa/inet.h>
 
-int main(int argc, char *argv[]) {
-
+int main(int argc, char *argv[]) 
+{
 	struct addrinfo hints, *res, *p;
 	int status;
 	char ipstr[INET6_ADDRSTRLEN];
 
-	if(argc != 2) {
+	if(argc != 2) 
+	{
 		fprintf(stderr, "usage: showip hostname\n");
 		return 1;
 	}
@@ -25,15 +26,16 @@ int main(int argc, char *argv[]) {
 	hints.ai_family = AF_UNSPEC;
 	hints.ai_socktype = SOCK_STREAM;
 
-	if((status = getaddrinfo(argv[1], NULL, &hints, &res)) != 0) {
+	if((status = getaddrinfo(argv[1], NULL, &hints, &res)) != 0) 
+	{
 		fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(status));
 		return 2;
 	}
 
 	printf("IP Addresses for %s:\n\n", argv[1]);
 
-	for(p = res; p != NULL; p = p->ai_next) {
-		
+	for(p = res; p != NULL; p = p->ai_next) 
+	{
 		void *addr;
 		char *ipver;
 
@@ -41,12 +43,16 @@ int main(int argc, char *argv[]) {
 		 * get the pointer to the address itself
 		 * different fields in IPv4 and IPv6:
 		 */
-		if(p->ai_family == AF_INET) { /* IPv4 */
+		if(p->ai_family == AF_INET) 
+		{ 
+			/* IPv4 */
 			struct sockaddr_in *ipv4 = (struct sockaddr_in*)p->ai_addr;
 			addr = &(ipv4->sin_addr);
 			ipver = "IPv4";
 		}
-		else { /* IPv6 */
+		else 
+		{ 
+			/* IPv6 */
 			struct sockaddr_in6 *ipv6 = (struct sockaddr_in6*)p->ai_addr;
 			addr = &(ipv6->sin6_addr);
 			ipver = "IPv6";
