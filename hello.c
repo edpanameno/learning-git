541c8ba17036e19dcdaac9185187e6f229c32897
Ed Panameno <ed.panameno@gmail.com>
2011-09-09 13:47:54 -0700

    Added a simple function to output your name.

--- hello.c
+++ hello.c
@@ -1,9 +1,20 @@
 #include <stdio.h>
 
+/**
+ * A simple function that will allow the user
+ * to print their name.
+ */
+void print_name(char *name)
+{
+	printf("Your name is: %s\n", name);
+}
+
 int main(int argc, char *argv[]) 
 {
 	printf("Hello, World!\n");
 	printf("Git is fun!\n");
+
+	print_name("edwin");
 	return 0;
 }
 
